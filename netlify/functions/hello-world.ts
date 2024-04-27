3c4145649a47210f90f080f2d22891491fb43dab
Roni Chabra <rchabra@tripactions.com>
2024-04-27 18:07:39 +0300

    hello-word function2

--- netlify/functions/hello-world.ts
+++ netlify/functions/hello-world.ts
@@ -1,9 +1,8 @@
-  export default async (req: Request) => {
+export default async (req: Request) => {
     console.log('SOME_SECOND_KEY', process.env.SOME_SECOND_KEY);
     console.log('TZ', process.env.TZ)
-    return Response.json({
-        statusCode: 200,
-        body: "hello world!"
-      });
-  }  
-  
+    return new Response(JSON.stringify({
+      statusCode: 200,
+      body: "hello world!"
+    }));
+  }
