aa9b80fda54cf000fdec84b5bb92f2b8275f5ec7
Roni Chabra <rchabra@tripactions.com>
2024-12-06 17:27:36 +0200

    chore: add form

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,15 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Form from './Form';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Welcome() {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const [showGoUp, setShowGoUp] = useState(false);
+  const [doneAnimation, setDoneAnimation] = useState(false);
 
   const { contextSafe } = useGSAP({ scope: containerRef });
 
@@ -13,15 +19,19 @@ export default function Welcome() {
       defaults: {
         duration: 3,
         ease: 'power3.out',
-      }
+      },
     })
-    .fromTo('.sky', {y:0},{y:-200}, 0)
-    .fromTo('.cloud1', {y:100},{y:-800}, 0)
-    .fromTo('.cloud2', {y:-150},{y:-500}, 0)
-    .fromTo('.cloud3', {y:-50},{y:-650}, 0)
-    .fromTo('.mountBg', {y:-10},{y:-100}, 0)
-    .fromTo('.mountMg', {y:-30},{y:-250}, 0)
-    .fromTo('.mountFg', {y:-50},{y:-600}, 0)
+      .fromTo('.sky', { y: 0 }, { y: -200 }, 0)
+      .fromTo('.cloud1', { y: 100 }, { y: -800 }, 0)
+      .fromTo('.cloud2', { y: -150 }, { y: -500 }, 0)
+      .fromTo('.cloud3', { y: -50 }, { y: -650 }, 0)
+      .fromTo('.mountBg', { y: -10 }, { y: -100 }, 0)
+      .fromTo('.mountMg', { y: -30 }, { y: -250 }, 0)
+      .fromTo('.mountFg', { y: -50 }, { y: -600 }, 0)
+    setShowGoUp(true);
+    setTimeout(() => {
+      setDoneAnimation(true);
+    }, 1200);
   });
   const mouseEnterHandler = contextSafe(() => {
     gsap.to('.arrow', { y: 10, duration: 0.8, ease: 'back.inOut(3)', overwrite: 'auto' })
@@ -31,78 +41,129 @@ export default function Welcome() {
     gsap.to('.arrow', { y: 0, duration: 0.5, ease: 'power3.out', overwrite: 'auto' })
   });
 
-  return (
-    <div ref={containerRef}  onMouseEnter={() => mouseEnterHandler()} onMouseLeave={() => mouseLeaveHandler()}>
-      <div className="scrollDist"></div>
-      <main>
-        <svg viewBox="0 0 1200 800" xmlns="http://www.w3.org/2000/svg">
-          <mask id="m">
-            <g className="cloud1">
-              <rect fill="#fff" width="100%" height="801" y="799" />
-              <image
-                href="https://assets.codepen.io/721952/cloud1Mask.jpg"
-                width="1200"
-                height="800"
-              />
-            </g>
-          </mask>
+  const handleGoUp = () => {
+    setShowGoUp(false);
+    setDoneAnimation(false);
+    gsap.timeline({
+      smoothChildTiming: true,
+      defaults: {
+        duration: 3,
+        ease: 'power3.out',
+      },
+    }).fromTo('.sky', { y: 0 }, { y: 0 })
+      .fromTo('.cloud1', { y: -800 }, { y: 100 }, 0)
+      .fromTo('.cloud2', { y: -500 }, { y: -150 }, 0)
+      .fromTo('.cloud3', { y: -650 }, { y: -50 }, 0)
+      .fromTo('.mountBg', { y: -100 }, { y: -10 }, 0)
+      .fromTo('.mountMg', { y: -250 }, { y: -30 }, 0)
+      .fromTo('.mountFg', { y: -600 }, { y: -50 }, 0);
 
-          <image
-            className="sky"
-            href="https://assets.codepen.io/721952/sky.jpg"
-            width="1200"
-            height="590"
-          />
-          <image
-            className="mountBg"
-            href="https://assets.codepen.io/721952/mountBg.png"
-            width="1200"
-            height="800"
-          />
-          <image
-            className="mountMg"
-            href="https://assets.codepen.io/721952/mountMg.png"
-            width="1200"
-            height="800"
-          />
-          <image
-            className="cloud2"
-            href="https://assets.codepen.io/721952/cloud2.png"
-            width="1200"
-            height="800"
-          />
-          <image
-            className="mountFg"
-            href="https://assets.codepen.io/721952/mountFg.png"
-            width="1200"
-            height="800"
-          />
-          <image
-            className="cloud1"
-            href="https://assets.codepen.io/721952/cloud1.png"
-            width="1200"
-            height="800"
-          />
-          <image
-            className="cloud3"
-            href="https://assets.codepen.io/721952/cloud3.png"
-            width="1200"
-            height="800"
-          />
-          <text fill="#fff" x="350" y="200">
-            EXPLORE
-          </text>
-          <polyline
-            className="arrow"
-            fill="#fff"
-            points="599,250 599,289 590,279 590,282 600,292 610,282 610,279 601,289 601,250"
-          />
+  };
+  // https://codepen.io/shunyadezain/details/poyEBLE
+  return (
+    <div ref={containerRef} onMouseEnter={() => mouseEnterHandler()} onMouseLeave={() => mouseLeaveHandler()}>
+      <div className="scrollDist"></div>
+      <main className='tracking-in-expand' >
+        <svg viewBox="0 0 1200 800" xmlns="http://www.w3.org/2000/svg">
+         { <>
+            <mask id="m">
+              <g className="cloud1">
+                <rect fill="#fff" width="100%" height="801" y="799" />
+                <image
+                  href="https://assets.codepen.io/721952/cloud1Mask.jpg"
+                  width="1200"
+                  height="800"
+                />
+              </g>
+            </mask>
+
+            <image
+              className="sky"
+              href="https://assets.codepen.io/721952/sky.jpg"
+              width="1200"
+              height="590"
+            />
+            <image
+              className="mountBg"
+              href="https://assets.codepen.io/721952/mountBg.png"
+              width="1200"
+              height="800"
+            />
+            <image
+              className="mountMg"
+              href="https://assets.codepen.io/721952/mountMg.png"
+              width="1200"
+              height="800"
+            />
+            <image
+              className="cloud2"
+              href="https://assets.codepen.io/721952/cloud2.png"
+              width="1200"
+              height="800"
+            />
+            <image
+              className="mountFg"
+              href="https://assets.codepen.io/721952/mountFg.png"
+              width="1200"
+              height="800"
+            />
+            <image
+              className="cloud1"
+              href="https://assets.codepen.io/721952/cloud1.png"
+              width="1200"
+              height="800"
+            />
+            <image
+              className="cloud3"
+              href="https://assets.codepen.io/721952/cloud3.png"
+              width="1200"
+              height="800"
+            />
+            <text fill="#fff" x="350" y="200">
+              EXPLORE
+            </text>
+            <polyline
+              className="arrow"
+              fill="#fff"
+              points="599,250 599,289 590,279 590,282 600,292 610,282 610,279 601,289 601,250"
+            />
+          </> }
 
           <g mask="url(#m)">
-            <rect fill="#fff" width="100%" height="100%" />
-            <text x="415" y="200" fill="#162a43">
-               NAVAN
-            </text>
+            {!doneAnimation && <rect fill="#fff" width="100%" height="100%"></rect>}
+            {doneAnimation && <><image
+              className="background-image tracking-in-expand"
+              href="https://www.lbr.co.il/TravelsPersonalArea/home-process-bg-6.b96cda387f100369b0bb.jpg"
+              width="1200"
+              height="800"
+            />
+              <defs>
+                <linearGradient id="combinedGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                  <stop offset="0%" stopColor="#ffffff" stopOpacity="1" />
+                  <stop offset="30%" stopColor="#ffffff" stopOpacity="0.5" />
+                  <stop offset="100%" stopColor="#ffffff" stopOpacity="0" />
+                </linearGradient>
+              </defs>
+              <rect
+                id="overlayRect"
+                x="0"
+                y="0"
+                width="1200"
+                height="800"
+                className="fade-container"
+                opacity="1"
+                fill="url(#combinedGradient)"
+              />
+            </>
+            }
+            <image
+              x="415"
+              y="100"
+              href="https://d2w7f1pl8j4yzn.cloudfront.net/logos/navan/navan-dynamic.svg"
+              width="370"
+              height="100"
+              style={{ filter: doneAnimation ? '' : 'invert(1)' }}
+            />
           </g>
 
           <rect
@@ -116,7 +177,36 @@ export default function Welcome() {
             style={{ cursor: 'pointer' }}
           />
         </svg>
+        {doneAnimation && <div className="form-container tracking-in-expand" style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          fontSize: "20px",
+          top: "25%"
+        }}>
+          <Form />
+
+
+        </div>}
       </main>
-    </div>
+      {
+        showGoUp && <button
+          onClick={() => handleGoUp()}
+          style={{
+            position: 'fixed',
+            bottom: '20px',
+            right: '20px',
+            padding: '10px 20px',
+            background: '#000',
+            color: '#fff',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+          }}
+        >
+          Go Up
+        </button>
+      }
+    </div >
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { StrictMode } from "react";
+import "~/styles/global.css";
 import "~/styles/welcome.css";
+import "~/styles/form.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return  <StrictMode><Component {...pageProps} /></StrictMode>;
