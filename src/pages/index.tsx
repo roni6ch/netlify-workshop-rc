aa002a2a9a8863f7e1082f1507eab3813f22dcc1
Roni Chabra <rchabra@tripactions.com>
2024-12-07 21:37:15 +0200

    chore: parallax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,78 @@
-import Link from "next/link";
-
-import Nav from "~/components/Nav";
-
+import { useEffect } from "react";
+import Rellax from "rellax";
+// https://dixonandmoe.com/rellax/#GettingStarted Rellax
 export default function Index() {
-  return (
-    <main>
-<form name="contact" method="POST" data-netlify="true">
-  <input type="hidden" name="form-name" value="contact" />
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-<form name="contact" data-netlify="true" hidden>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-      <Nav title="Netlify Workshop" />
-      <section className="flex columns">
-        <div>
-          <h2>Rendering strategies</h2>
-          <ul>
-            <li>
-              <Link href="/rendering-strategies/ssg">
-                Static site generation (SSG)
-              </Link>
-            </li>
-            <li>
-              <Link href="/rendering-strategies/ssr">
-                Server-side rendering (SSR)
-              </Link>
-            </li>
-            <li>
-              <Link href="/rendering-strategies/swr">
-                Stale-while-revalidate (SWR)
-              </Link>
-            </li>
+  useEffect(() => {
+    new Rellax('.rellax');
+  }, []);
+  return <>
+    <section className="section section-top">
+      <div className="content rellax" data-rellax-speed="5">
+        <h1>Create your company account</h1>
+        <a href="#" className="btn btn-primary">Learn More</a>
+      </div>
+    </section>
 
-            <li>
-              <Link href="/rendering-strategies/odr">
-                On-demand revalidation (ODR)
-              </Link>
-            </li>
-          </ul>
-          <h2>Redirects and rewrites</h2>
-          <ul>
-            <li>
-              <Link href="/redirect-example">Redirect example</Link>
-            </li>
-            <li>
-              <Link href="/preview-image">Rewrite example</Link>
-            </li>
-          </ul>
+    <section className="section section-stream">
+      <img
+        className="play rellax"
+        src="https://d2w7f1pl8j4yzn.cloudfront.net/rewards/rewards-education-bonvoy-pts.webp"
+        alt=""
+        data-rellax-speed="-1" data-rellax-xs-speed="-5"
+      />
+      <div className="content rellax" data-rellax-speed="10">
+        <div>
+          <h2 className="secondary-text">Exclusive travel discounts
+          </h2>
+          <p>
+          Book at competitive rates, earn third-party loyalty points and discounts
+          </p>
         </div>
         <div>
-          <h2>Core primitives examples</h2>
-          <h3>Functions</h3>
-          <ul>
-            <li>
-              <Link href="/primitives/functions/proxy">Proxy to APIs</Link>
-            </li>
-            <li>
-              <Link href="/primitives/functions/combine-and-filter">
-                Combine and filter API responses
-              </Link>
-            </li>
-            <li>
-              <Link href="/primitives/functions/streams">Streams</Link>
-            </li>
-          </ul>
-          <h3>Edge functions</h3>
-          <ul>
-            <li>
-              <Link href="/primitives/edge-functions/ab-testing">
-                A/B testing
-              </Link>
-            </li>
-            <li>
-              <Link href="/primitives/edge-functions/geolocation">
-                Geolocation
-              </Link>
-            </li>
-          </ul>
-          <h3>Blobs</h3>
-          <ul>
-            <li>
-              <Link href="/primitives/blobs/blobs">Access blob storage</Link>
-            </li>
-          </ul>
-          <h3>Image CDN</h3>
-          <ul>
-            <li>
-              <Link href="/primitives/image-cdn/same-origin">
-                Same-origin images
-              </Link>
-            </li>
-            <li>
-              <Link href="/primitives/image-cdn/remote">Remote images</Link>
-            </li>
-          </ul>
+          <h2 className="secondary-text">Best-in-class support
+          </h2>
+          <p>
+          Skip long support lines and get free, personalized travel support 24/7
+          </p>
         </div>
-      </section>
-    </main>
-  );
+      </div>
+    </section>
+
+    <section className="section section-grid">
+      <div className="rellax" data-rellax-speed="1" data-rellax-xs-speed="3">
+        <i className="fas fa-video fa-3x secondary-text"></i>
+        <h2>Navan app
+        <span className="secondary-text dot">.</span></h2>
+        <p>
+        Download the Navan app to manage your bookings and get real-time travel updates on the go.
+        </p>
+      </div>
+      <div className="rellax" data-rellax-speed="4" data-rellax-xs-speed="3">
+        <i className="fas fa-users fa-3x secondary-text"></i>
+        <h2>Travel is free!</h2>
+        <p>
+        Say goodbye to surprise charges and unexpected costs
+        </p>
+      </div>
+      <div className="rellax" data-rellax-speed="7" data-rellax-xs-speed="3">
+        <i className="fas fa-book fa-3x secondary-text"></i>
+        <h2>
+        Expense is free</h2>
+        <p>
+        for the first 3 active users
+        Then it’s only $10 a month per additional active user.
+        </p>
+      </div>
+    </section>
+
+    <footer className="footer">
+      <ul>
+        <li><a href="#">Faq</a></li>
+        <li><a href="#">Terms of Use</a></li>
+        <li><a href="#">Privacy Notice</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">About Us</a></li>
+      </ul>
+    </footer>
+  </>;
 }
